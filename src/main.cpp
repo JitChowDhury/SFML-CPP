dff291c59fd2277919fe5f613a13ef782e50e658
JitChowDhury <jitchowdhury8717@gmail.com>
2025-07-30 10:50:29 +0530

    Sprite and textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <filesystem>
 
 int main()
 {
+std::cout << "Working Directory: " << std::filesystem::current_path() << std::endl;
     sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "My SFML");
-    sf::RectangleShape shape(sf::Vector2f(100,50));
-    shape.setFillColor(sf::Color::Green);
+  /*  sf::RectangleShape shape(sf::Vector2f(100,50));*/
+
+	sf::Texture texture;
+	texture.loadFromFile("./main.png");
+    sf::Sprite sprite(texture);
+	sprite.setScale(0.1f, 0.1f); 
+
+    //shape.setFillColor(sf::Color::Green);
+
+    float xPos{ 0.0f };
 
     while (window.isOpen())
     {
 		sf::Event event;
+   
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -22,13 +33,27 @@ int main()
                 if (event.key.code == sf::Keyboard::W)
                 {
 
-                std::cout << "A Key is Pressed" << std::endl;
+                std::cout << "A Key is Pressed" << std::endl;  
                 }
             }
+
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+            {
+                std::cout << "Left Mouse Button Pressed" << std::endl;
+            }
+
+        }
+
+        sprite.setPosition(xPos, 0.0f);
+        xPos+=0.001f;
+        if (xPos > 400)
+        {
+            xPos = 0;
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 }
