7673e0a9729e2ff99f159b02dcf0b321dcbbbd0f
JitChowDhury <jitchowdhury8717@gmail.com>
2025-08-03 09:35:02 +0530

    Start Day 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
-#include "Game.h"
+//#include "Game.h"
+//
+//int main()
+//{
+//	Game game;
+//	game.run();
+//}
+#include<iostream>
+#include <filesystem>
+#include<SFML\Graphics.hpp>
 
 int main()
 {
-	Game game;
-	game.run();
-}
+	sf::RenderWindow window(sf::VideoMode(1240, 720), "TEST SFML");
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("assets/player.png"))
+	{
+		std::cout << "PLayer Texture Not Found" << std::endl;
+	
+		return -1;
+	}
+
+
+	sf::Sprite playerSprite;
+	playerSprite.setTexture(playerTexture);
+	playerSprite.setPosition(sf::Vector2f(400.f, 500.f));
+
+
+
+	
+		while (window.isOpen())
+		{
+			sf::Event event;
+				while (window.pollEvent(event))
+				{
+					if (event.type == sf::Event::Closed)
+					{
+						window.close();
+					}
+				}
+
+
+
+				window.clear();
+				window.draw(playerSprite);
+				window.display();
+		}
+	}
